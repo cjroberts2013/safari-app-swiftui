e7335c3968ad4a005e969c69c566fbcae154fa90
Charles Roberts <cjroberts2013@gmail.com>
2022-06-28 20:12:34 -0400

    Initial Commit

---
+++ Safari-app-swiftui/Safari_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Safari_app_swiftuiApp.swift
+//  Safari-app-swiftui
+//
+//  Created by Charles Roberts on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Safari_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
