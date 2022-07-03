ab9b76e9a0232f36393daf1b5d392f8cc6853652
Charles Roberts <cjroberts2013@gmail.com>
2022-07-03 14:07:52 -0400

    More features for map section

---
+++ Safari-app-swiftui/Model/LocationModel.swift
@@ -0,0 +1,21 @@
+//
+//  LocationModel.swift
+//  Safari-app-swiftui
+//
+//  Created by Charles Roberts on 7/2/22.
+//
+
+import Foundation
+import MapKit
+
+struct Location: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
