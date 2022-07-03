539250ac87256640b5cb50b478e707ebae08c854
Charles Roberts <cjroberts2013@gmail.com>
2022-07-03 15:59:56 -0400

    Finished project

--- Safari-app-swiftui/App/GalleryView.swift
+++ Safari-app-swiftui/App/GalleryView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptic = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+    
     var body: some View {
-        Text("Gallery")
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack (alignment: .center, spacing: 30) {
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(12)
+                
+                Slider(value: $gridColumn, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn) { newValue in
+                        gridSwitch()
+                    }
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { animal in
+                        Image(animal.image)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(8)
+                            .onTapGesture {
+                                selectedAnimal = animal.image
+                                haptic.impactOccurred()
+                            }
+                    }
+                }
+                .animation(.easeIn)
+                .onAppear {
+                    gridSwitch()
+                }
+            }
+            .padding(.horizontal, 10)
+            .padding(.vertical, 15)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(MotionAnimationView())
     }
 }
 
